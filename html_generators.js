b988f680b1177bdc291c2220393760d9f458922b
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2022-11-06 08:07:31 -0800

    Adding font importing

--- html_generators.js
+++ html_generators.js
@@ -25,6 +25,12 @@ HtmlGenerator['divclass'] = function(block) {
   var code = `class="${text_text}"`;
   return code;
 };
+HtmlGenerator['import font'] = function(block) {
+  var text_text = block.getFieldValue('text');
+  // TODO: Assemble JavaScript into code variable.
+  var code = `<link rel="stylesheet" href="${text_text}">`;
+  return code;
+};
 
 HtmlGenerator['baseframe'] = function(block) {
   var statements_head = HtmlGenerator.statementToCode(block, 'head');
