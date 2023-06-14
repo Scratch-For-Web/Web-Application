ae7e236cfb1d05471e5fe749f3a985852e56b647
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2023-06-13 18:38:19 -0700

    Delete modules directory

--- modules/blockly/generator/react_gen.js
+++
@@ -1,2 +0,0 @@
-let jsx_base = ``
-
--- modules/blockly/main/imports.js
+++
@@ -1,4 +0,0 @@
-import "../generator/react_gen.js"
-import "../generator/html_blocks_gen.js"
-import "../blocks/html/definitions.js"
-import "../../auth/authcheck.js"
